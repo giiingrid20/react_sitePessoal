bbf2f2f2d28ac0a8f168d4312e060ab5ae311c0d
giiingrid20 <giovanna.ingrid@escolar.ifrn.edu.br>
2024-09-13 09:51:04 -0300

    continuando estudos...

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,19 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
-
+import HelloWorld from './components/HelloWorld';
+import Frase from './components/Frase';
 function App() {
   const name = 'mateus'
 
   function sum(a, b){
     return a + b
   }
+
+  const url = "https://via.placeholder.com/150"
   return (
     <div className="App">
       <h1> ola react</h1>
       <p>ola, {name}</p>
       <p>soma: {sum(3, 3)}</p>
+      <img src={url} alt="minha imagem"/>
+      <HelloWorld></HelloWorld>
+      <Frase></Frase>
     </div>
   );
 }
+
+
 
 export default App;
---
+++ meu-projeto/src/components/Frase.js
@@ -0,0 +1,7 @@
+function Frase() {
+    return (
+        <div>Esta é minha primeira frase como componete</div>
+    )
+}
+
+export default Frase
